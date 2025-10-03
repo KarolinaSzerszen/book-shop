734f8c595490da90dbc45a4890215d888d4af7e4
Karol <szerszenk56@gmail.com>
2025-10-03 16:16:38 +0200

    centerMode:false

--- client/src/components/SimpleSlider.jsx
+++ client/src/components/SimpleSlider.jsx
@@ -38,8 +38,8 @@ const TailwindSlider = ({ books = [], isLoading = false, name, img }) => {
       { breakpoint: 1440, settings: { slidesToShow: 5 } },
       { breakpoint: 1200, settings: { slidesToShow: 4 } },
       { breakpoint: 1024, settings: { slidesToShow: 3 } },
-      { breakpoint: 768, settings: { slidesToShow: 2 } },
-      { breakpoint: 480, settings: { slidesToShow: 1 } },
+      { breakpoint: 768, settings: { slidesToShow: 2, centerMode: false } },
+      { breakpoint: 480, settings: { slidesToShow: 1, centerMode: false } },
     ],
   };
 
