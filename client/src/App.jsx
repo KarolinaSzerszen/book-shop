ad9d408e025f68aec1addd156b4adc3aada1613a
Karol <szerszenk56@gmail.com>
2025-12-09 17:10:59 +0100

    enable analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="px-4">
+      <Analytics />
       <Navbar />
     </div>
   );
